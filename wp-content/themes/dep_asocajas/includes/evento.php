12acbdb0bfbbec2c3b110a12f29b99b1981cb9a8
cristian parra <cdpsk8@gmail.com>
2019-04-03 20:27:47 -0500

    pagina evento quemada a codigo

---
+++ wp-content/themes/dep_asocajas/includes/evento.php
@@ -0,0 +1,251 @@
+<section id="evento">
+  <div class="container-fluid">
+    <div class="row">
+      <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/Banner-foro-sin-boton.png" alt="" width="100%" height="auto">
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h2>Hablemos de Protección Social en Colombia</h2>
+        <p>Desde la creación del Sistema de Compensación Familiar, hace 65 años, hemos contribuido al cierre de brechas sociales y al mejoramiento de la calidad de vida de los trabajadores y sus familias, brindándole acceso a educación, salud, vivienda, turismo, cultura y recreación, entre otros. Por esto, conscientes de los retos que afronta el país en materia de Protección Social, nos unimos con el Ministerio del Trabajo para promover un análisis y debate sobre esta temática, que incide directamente en el bienestar de todos los colombianos.
+
+Bajo este propósito realizaremos el próximo 29 de mayo el foro Hablemos de Protección Social en Colombia, un espacio en el que articularemos a altos funcionarios del Gobierno Nacional, expertos nacionales e internacionales, líderes gremiales, empresarios y trabajadores alrededor de un Sistema que se relaciona con la productividad y competitividad del país.
+
+Aspectos como la movilidad social, redistribución del ingreso, calidad en la prestación del servicio de salud, formación para el trabajo, garantías de bienestar, oportunidades para los trabajadores y desafíos empresariales tendrán un espacio en la agenda académica.
+
+Lo invitamos a participar en el Foro que permitirá trazar una hoja de ruta para fortalecer la Protección y Seguridad Social en Colombia a partir de diferentes visiones.
+
+¡Los esperamos!
+
+
+Adriana María Guillén Arango
+Presidente Ejecutiva, Asocajas
+</p>
+      </div>
+      <div class="col-md-4">
+        <div class="detalles" style="background-color:#f6f6f6; padding:20px;">
+          <h2 class="text-center">DETALLES</h2>
+          <p style="padding:10px"> <b style="margin-right:10px;">Hora:</b>7:30 </p>
+            <p style="padding:10px"> <b>Fecha:</b>29 de mayo </p>
+              <p style="padding:10px"> <b>Hora:</b>Club El Nogal - Gran salón</p>
+                <p style="padding:10px"> <b>Precio:</b>$500.000 + iva</p>
+        </div>
+        <div style="background-color:#00c6f4;">
+          <a class="text-center" href="#"> <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/icono-carro.png" alt=""> <p>COMPRAR ENTRADAS</p> </a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid text-center">
+    <h2>PONENTES</h2>
+    <div class="row">
+
+      <div class="col-md-1">
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/8.png" alt="">
+          <h4>Adriana María Guillén Arango</h4>
+          <i>Presidente Ejecutiva, Asocajas</i>
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/1.png" alt="">
+          <h4>Iván Duque Márquez</h4>
+          <i>Presidente de la República de Colombia</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/2.png" alt="">
+          <h4>Alberto Carrasquilla Barrera </h4>
+          <i>Ministro de Hacienda y Crédito Público</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/3.png" alt="">
+          <h4>Alicia Arango Olmos</h4>
+          <i>Ministra del Trabajo</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/4.png" alt="">
+          <h4>Jonathan Malagón González</h4>
+          <i>Ministro de Vivienda </i>
+
+
+      </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col-md-1">
+
+      </div>
+      <div class="col-md-2">
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/5.png" alt="">
+          <h4>Juan Pablo Uribe Restrepo</h4>
+          <i>Ministro de Salud y Protección Social</i>
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/6.png" alt="">
+          <h4>José Manuel Restrepo Abondano </h4>
+          <i>Ministro de Comercio, Industria y Turismo</i>
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/7.png" alt="">
+          <h4>Andrés Uribe Medina </h4>
+          <i>Viceministro, Empleo y Pensiones </i>
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/9.png" alt="">
+          <h4>Rafael de la Cruz </h4>
+          <i>Gerente General, Departamento de Países del Grupo Andino</i>
+
+      </div>
+        <div class="col-md-2">
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/10.png" alt="">
+          <h4>Bruce Mac Master Rojas </h4>
+          <i>Presidente, Asociación Nacional de Empresarios de Colombia, ANDI</i>
+
+
+      </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col-md-1">
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/11.png" alt="">
+          <h4>Carlos Mario Giraldo Moreno </h4>
+          <i>Presidente Grupo Éxito </i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/12.png" alt="">
+          <h4>Juan Lozano Ramírez </h4>
+          <i>Abogado y Periodista</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/13.png" alt="">
+          <h4>José Antonio Ocampo</h4>
+          <i>Coodirector Banco de la República  </i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/14.png" alt="">
+          <h4>Angel Melguizo </h4>
+          <i>VicePresidente de Asuntos Externos y Regulatorios, DirecTV </i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/15.png" alt="">
+          <h4>Oscar Cabrera Izquierdo </h4>
+          <i>Presidente Ejecutivo de BBVA Colombia</i>
+
+      </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col-md-1">
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/16.png" alt="">
+          <h4>Alfonso Gómez Palacio </h4>
+          <i>Presidente de la unidad Telefónica Hispam Norte</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/17.png" alt="">
+          <h4>Eduardo Pizano de Narváez</h4>
+          <i>Escuela de Gobierno, Universidad de los Andes</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/18.png" alt="">
+          <h4>Carlos Angulo Galvis </h4>
+          <i>Fundación Corona</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/19.png" alt="">
+          <h4>María Margarita Ruiz Rodgers </h4>
+          <i>Directora, Proyectos Sociales y Económicos, Asocajas</i>
+
+
+      </div>
+      <div class="col-md-2">
+
+          <img src="http://www.asocajas.org.co/wp-content/uploads/2019/04/20.png" alt="">
+          <h4>Mauricio Reina Echeverri </h4>
+          <i>Abogado y periodista</i>
+
+
+      </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+
+  </div>
+  <div class="panel-heading container">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse1" class="collapsed" aria-expanded="false">AGENDA <span class="glyphicon glyphicon-chevron-down"></span></a>
+        </h4>
+      </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+    <p>  7:30 a.m.</p>
+    </div>
+    <div class="col-md-8">
+      <p>registro de participantes</p>
+      <h3 class="text-center">SESIÓN DE INSTALACION</h3>
+      <hr style="background-image:linear-gradient(to right, rgb(55, 152, 214) , rgba(64, 172, 99, 0.6)); height:3px; width:40%; "  />
+    </div>
+  </div>
+
+</div>
+</section>
---
+++ wp-content/themes/dep_asocajas/templates/_evento-tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  Template Name: Evento Template
+*/
+
+	get_template_part('includes/header');
+
+	get_template_part('includes/evento');
+
+	get_template_part('includes/footer');
