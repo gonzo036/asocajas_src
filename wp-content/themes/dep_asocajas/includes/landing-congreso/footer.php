e9267833c1f74509c98b2ed922c0350c4b6fd0a3
Camilo Vanegas <Camilo.Vanegas@vml.com>
2019-07-23 17:32:37 -0500

    CSS/Footer fix layout and colors

--- wp-content/themes/dep_asocajas/includes/landing-congreso/footer.php
+++ wp-content/themes/dep_asocajas/includes/landing-congreso/footer.php
@@ -1,17 +1,17 @@
 <section>
   <div class="container-fluid congreso-footer">
-    <div class="row">
-      <div class="col-md-3">
+    <div class="congreso-footer__wrapper">
+      <div class="congreso-footer__wrapper-item">
         <img src="https://www.asocajas.org.co/wp-content/uploads/2019/05/Logo-asocajas.png" alt="" width="236px" height="auto">
       </div>
-      <div class="col-md-3">
+      <div class="congreso-footer__wrapper-item">
         <p>Calle 94 No 11-30, piso 6</p>
         <p>Bogotá Colombia</p>
       </div>
-      <div class="col-md-3">
+      <div class="congreso-footer__wrapper-item">
         <p>comunicaciones@asocajas.org.co</p>
       </div>
-      <div class="col-md-3">
+      <div class="congreso-footer__wrapper-item">
         <p>(57 1) 3232765</p>
       </div>
     </div>
