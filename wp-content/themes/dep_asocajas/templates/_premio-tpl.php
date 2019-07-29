53d25adfed6e577db641805943f2edcaab545d5a
cristian parra <cdpsk8@gmail.com>
2019-07-29 18:00:37 -0500

    back landing premio 1.0

---
+++ wp-content/themes/dep_asocajas/templates/_premio-tpl.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  Template Name: Landing Premio Template
+*/
+
+get_template_part('includes/landing-premio/header');
+
+get_template_part('includes/landing-premio/linea-tiempo');
+
+get_template_part('includes/landing-premio/objetivo');
+
+get_template_part('includes/landing-premio/por-que');
+
+get_template_part('includes/landing-premio/sobre-galardon');
+
+get_template_part('includes/landing-premio/footer');
+
+ ?>
