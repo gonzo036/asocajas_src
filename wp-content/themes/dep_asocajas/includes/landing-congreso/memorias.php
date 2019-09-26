ba10652c2641bb70c85cc35a720e6d496f4bb1f3
unknown <cdpsk8@gmail.com>
2019-09-26 18:20:10 -0500

    back end memorias

--- wp-content/themes/dep_asocajas/includes/landing-congreso/memorias.php
+++ wp-content/themes/dep_asocajas/includes/landing-congreso/memorias.php
@@ -20,113 +20,134 @@
          <h2>memorias</h2>
        </header>
      </div>
-     <div class="memorias_content">
-       <ul class="nav nav-pills">
-         <?php
-          $cont=0;
-           while(have_rows('memoria')): the_row();
 
-           $cont=$cont +1;
+      <div class="memoria-item">
+        <?php
+
+          while(have_rows('memoria')): the_row();
+          $ano = get_sub_field('ano');
          ?>
-         <?php if ($cont==1): ?>
+        <div class="">
+          <h3><?php the_sub_field('ano'); ?></h3>
+        </div>
+       <div class="memorias_content">
+         <ul class="nav nav-pills">
+           <?php
+            $cont=0;
+             while(have_rows('memorias')): the_row();
 
-          <li class="active"><a data-toggle="pill" href="#menu<?php echo $cont ?> "><?php the_sub_field('dia_fecha'); ?></a></li>
+             $cont=$cont +1;
+           ?>
+           <?php if ($cont==1): ?>
 
-        <?php else: ?>
-         <li><a data-toggle="pill" href="#menu<?php echo $cont; ?>"><?php the_sub_field('dia_fecha'); ?></a></li>
-         <?php
-          endif;
-         ?>
-         <?php
-           endwhile;
-         ?>
+            <li class="active"><a data-toggle="pill" href="#menu<?php echo $cont ?><?php echo $ano; ?>"><?php the_sub_field('dia_fecha'); ?></a></li>
 
-       </ul>
-
-       <div class="tab-content">
-
-         <?php
-         $cont=0;
-         $active='in active';
-           while(have_rows('memoria')): the_row();
-
-           $cont=$cont +1;
-         ?>
-         <?php if ($cont==1): ?>
-           <div id="menu<?php echo $cont;  ?>" class="tab-pane fade <?php echo $active; ?>">
-              <?php
-                while(have_rows('contenido')): the_row();
-                $titulo = get_sub_field('titulo');
-                $descripcion = get_sub_field('descripcion');
-                $youtube_id = get_sub_field('youtube_id');
-                $galeria = get_sub_field('galeria');
-
-               ?>
-                   <div class="item-memoria">
-                       <header>
-                         <h4><?php echo $titulo; ?></h4>
-                       </header>
-                         <?php echo $descripcion; ?>
-                         <div class="links-popup">
-                           <?php
-                            if($youtube_id):
-                            ?>
-                            <a href="#">ver video</a>
-                            <?php endif; ?>
-                             <?php
-                             if($galeria):
-                              ?>
-                              <a href="#">ver galeria</a>
-                            <?php endif; ?>
-                         </div>
-                    </div>
-                    <?php
-                      endwhile;
-                    ?>
-           </div>
-
-         <?php else: ?>
-           <div id="menu<?php echo $cont;  ?>" class="tab-pane fade">
-              <?php
-                while(have_rows('contenido')): the_row();
-                $titulo = get_sub_field('titulo');
-                $descripcion = get_sub_field('descripcion');
-                $youtube_id = get_sub_field('youtube_id');
-                $galeria = get_sub_field('galeria');
-
-               ?>
-                   <div class="item-memoria">
-                       <header>
-                         <h4><?php echo $titulo; ?></h4>
-                       </header>
-                         <?php echo $descripcion; ?>
-                         <div class="links-popup">
-                           <?php
-                            if($youtube_id):
-                            ?>
-                            <a href="#">ver video</a>
-                            <?php endif; ?>
-                             <?php
-                             if($galeria):
-                              ?>
-                              <a href="#">ver galeria</a>
-                            <?php endif; ?>
-                         </div>
-                    </div>
-                    <?php
-                      endwhile;
-
-                    ?>
-           </div>
+          <?php else: ?>
+           <li><a data-toggle="pill" href="#menu<?php echo $cont; ?><?php echo $ano; ?>"><?php the_sub_field('dia_fecha'); ?></a></li>
            <?php
             endif;
            ?>
            <?php
-            endwhile;
+             endwhile;
            ?>
 
-       </div>
+         </ul>
+         <?php
+           endwhile;
+         ?>
+         <div class="tab-content">
 
-     </div>
+           <?php
+            while(have_rows('memoria')): the_row();
+            $ano2 = get_sub_field('ano');
+           $cont=0;
+           $active='in active';
+             while(have_rows('memorias')): the_row();
+
+             $cont=$cont +1;
+           ?>
+           <?php if ($cont==1): ?>
+             <div id="menu<?php echo $cont;  ?><?php echo $ano2; ?>" class="tab-pane fade <?php echo $active; ?>">
+                <?php
+                  while(have_rows('contenido')): the_row();
+                  $titulo = get_sub_field('titulo');
+                  $descripcion = get_sub_field('descripcion');
+                  $youtube_id = get_sub_field('youtube_id');
+                  $galeria = get_sub_field('galeria');
+
+                 ?>
+                     <div class="item-memoria">
+                         <header>
+                           <h4><?php the_sub_field('titulo'); ?></h4>
+                         </header>
+                           <?php echo $descripcion; ?>
+                           <div class="links-popup">
+                             <?php
+                              if($youtube_id):
+                              ?>
+                              <a href="#">ver video</a>
+                              <?php endif; ?>
+                               <?php
+                               if($galeria):
+                                ?>
+                                <a href="#">ver galeria</a>
+                              <?php endif; ?>
+                           </div>
+                      </div>
+                      <?php
+                        endwhile;
+                      ?>
+             </div>
+
+           <?php else: ?>
+             <div id="menu<?php echo $cont;  ?><?php echo $ano2; ?>" class="tab-pane fade">
+                <?php
+                  while(have_rows('contenido')): the_row();
+                  $titulo = get_sub_field('titulo');
+                  $descripcion = get_sub_field('descripcion');
+                  $youtube_id = get_sub_field('youtube_id');
+                  $galeria = get_sub_field('galeria');
+
+                 ?>
+                     <div class="item-memoria">
+                         <header>
+                           <h4><?php echo $titulo; ?></h4>
+                         </header>
+                           <?php echo $descripcion; ?>
+                           <div class="links-popup">
+                             <?php
+                              if($youtube_id):
+                              ?>
+                              <a href="#">ver video</a>
+                              <?php endif; ?>
+                               <?php
+                               if($galeria):
+                                ?>
+                                <a href="#">ver galeria</a>
+                              <?php endif; ?>
+                           </div>
+                      </div>
+                      <?php
+                        endwhile;
+
+                      ?>
+
+             </div>
+             <?php
+              endif;
+             ?>
+             <?php
+              endwhile;
+             ?>
+
+
+         </div>
+         <?php
+           endwhile;
+
+         ?>
+       </div>
+      </div>
+
    </div>
  </section>
