ee7837fdde942036ccb621fe267ddb59b4c5cd06
Camilo <kmilo.vanegas@gmail.com>
2020-07-22 04:56:03 -0500

    Variables and new fonts support

--- wp-content/themes/dep_asocajas/functions/enqueues.php
+++ wp-content/themes/dep_asocajas/functions/enqueues.php
@@ -13,6 +13,10 @@ function bst_enqueues() {
 	wp_register_style('fancy-css', get_template_directory_uri() . '/css/jquery.fancybox.min.css', false, null);
 	wp_enqueue_style('fancy-css');
 
+	wp_enqueue_style( 'open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap', false );
+
+	wp_enqueue_style( 'open-sans', 'https://fonts.googleapis.com/css2?family=Lato:wght@700;900&display=swap', false );
+
 	wp_register_style('main-css', get_template_directory_uri() . '/assets/css/theme.css', false, null);
 	wp_enqueue_style('main-css');
 
